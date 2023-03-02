065728d7c239fff86638b6a41660a57579836c37
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-03-02 10:59:30 -0500

    js syntax edited and now working, added a couple lines of css

--- assets/script.js
+++ assets/script.js
@@ -3,18 +3,18 @@ var mainPage = document.querySelector("#main-page");
 var mainElement = document.querySelector("main");
 var startQuizButton = document.querySelector("#start-quiz");
 
-timer.setAttribute("style", "float: right");
-mainElement.setAttribute("style", "text-align: center")
+// timer.setAttribute("style", "float: right;");
 
 startQuizButton.addEventListener("click", function(event){
     event.preventDefault();
 
+    //starting page is removed
     mainPage.remove();
-    // console.log(mainPage.remove())
 
-    // var question1 = document.createElement(<div></div>);
-    // question1.textContent("TESTERRRRRRRR")
-    // document.body.children[1].appendChild(question1);
+    //question 1 is appended here 
+    var question1 = document.createElement("div");
+    question1.textContent = "TESTERRRRRRRR";
+    document.body.children[1].appendChild(question1);
 
 
 })
