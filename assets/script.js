29681a8f96b61739aa3876d869303ff45dfc8eff
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-03-02 09:25:28 -0500

    click event is not removing the first <div> element, trying my options

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,17 @@
 var mainPage = document.querySelector('#main-page');
 var startQuizButton = document.querySelector('#start-quiz');
 
-startQuizButton.addEventListener('click', function(event){
+
+
+startQuizButton.addEventListener("click", function(event){
     event.preventDefault();
 
     mainPage.remove();
+    // console.log(mainPage.remove())
 
-    var question1 = document.createElement(<div></div>);
-    
-    document.body.children[1].appendChild(question1);
+    // var question1 = document.createElement(<div></div>);
+    // question1.textContent("TESTERRRRRRRR")
+    // document.body.children[1].appendChild(question1);
 
 
 })
