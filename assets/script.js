7d58730b354edc570e1d21ba56191441dcc27892
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-03-02 14:11:39 -0500

    got my timer to worrrrrrk (at least the basics)

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var timer = document.querySelector(".timer");
-var mainPage = document.querySelector("#main-page");
+var secondsLeft = 75;
+var startingPage = document.querySelector("#main-page");
 var mainElement = document.querySelector("main");
 var startQuizButton = document.querySelector("#start-quiz");
 
@@ -9,14 +10,43 @@ startQuizButton.addEventListener("click", function(event){
     event.preventDefault();
 
     //starting page is removed
-    mainPage.remove();
+    startingPage.remove();
 
     //question 1 is appended here 
     var question1 = document.createElement("div");
     question1.textContent = "TESTERRRRRRRR";
     document.body.children[1].appendChild(question1);
 
+    //timer starts @75 seconds
+    startTimer();
 
 })
 
+function startTimer() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timer.textContent = "Time: " + secondsLeft;
+  
+    //   if (timerCount >= 0) {
+    //     // Tests if win condition is met
+    //     if (isWin && timerCount > 0) {
+    //       // Clears interval and stops timer
+    //       clearInterval(timerInterval);
+    //       winGame();
+    //     }
+    //   }
+    //   // Tests if time has run out
+    //   if (timerCount === 0) {
+    //     // Clears interval
+    //     clearInterval(timerInterval);
+    //     loseGame();
+    //   }
+  
+    }, 1000);
+}
+
+function startQuiz() {
+
+}
 
