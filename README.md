52d502df877e72ef7607b5a8c7c17b4ae2007bde
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-03-05 20:05:12 -0500

    office hours help: objects and forLoops

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # c4_code-quiz
 Making a code quiz from scratch for bootcamp users to use to study and track their scores
+
+
+Links used:
+
+element.remove()
+https://developer.mozilla.org/en-US/docs/Web/API/Element/remove
+
--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,23 @@ var startingPage = document.querySelector("#main-page");
 var mainElement = document.querySelector("main");
 var startQuizButton = document.querySelector("#start-quiz");
 
+
+var questions = [
+    {
+        title: "", 
+        choices: [], 
+        answer: 'answer here'
+    }, 
+    {
+        title: "", 
+        choices: []
+    }, 
+    {
+        title: "", 
+        choices: []
+    }
+]
+
 // timer.setAttribute("style", "float: right;");
 
 startQuizButton.addEventListener("click", function(event){
@@ -43,16 +60,30 @@ function startTimer() {
 }
 
 function question1() {
+
+    // TODO:
+    // get the current question object from the above array. 
+    var currentQuestion;
+
+
+    //  TODO: update the dom with the question
+
+
+    // TODO: Loop over the choices in the current question:
+    
     //question 1 is appended here 
     var question1 = document.createElement("section");
 
     var q1Title = document.createElement("h2");
     var answers = document.createElement("div");
 
+    //forloop this
     var answer1 = document.createElement("button");
     var answer2 = document.createElement("button");
     var answer3 = document.createElement("button");
     var answer4 = document.createElement("button");
+
+
 
     //text component of the page
     q1Title.textContent = "Commonly used data types DO NOT include:";
@@ -70,7 +101,48 @@ function question1() {
     document.body.children[1].children[0].children[1].appendChild(answer3);
     document.body.children[1].children[0].children[1].appendChild(answer4);
 
+   //call next question with right or wrong answer
 
 
 }
 
+function question2() {
+    //question 2 is appended here 
+    var question1 = document.createElement("section");
+
+    var q1Title = document.createElement("h2");
+    var answers = document.createElement("div");
+
+    var answer1 = document.createElement("button");
+    var answer2 = document.createElement("button");
+    var answer3 = document.createElement("button");
+    var answer4 = document.createElement("button");
+
+    //text component of the page
+    q1Title.textContent = "The condition in an if / else statement is enclosed within___.";
+    answer1.textContent = "1. quotes";
+    answer2.textContent = "2. curly brackets";//this is the right answer
+    answer3.textContent = "3. parentheses";
+    answer4.textContent = "4. square brackets";
+
+    //document.body.main.section.div
+    document.body.children[1].appendChild(question1);
+    document.body.children[1].children[0].appendChild(q1Title);
+    document.body.children[1].children[0].appendChild(answers);
+    document.body.children[1].children[0].children[1].appendChild(answer1);
+    document.body.children[1].children[0].children[1].appendChild(answer2);
+    document.body.children[1].children[0].children[1].appendChild(answer3);
+    document.body.children[1].children[0].children[1].appendChild(answer4);
+
+   //call next question with right or wrong answer
+
+
+}
+
+
+
+function answers() {
+
+    // if ()
+
+}
