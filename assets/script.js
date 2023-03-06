51f577c3eaf96c5058f90d91d5a967bfec0a96d7
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-03-05 20:33:01 -0500

    added all questions into an array of objects

--- assets/script.js
+++ assets/script.js
@@ -7,19 +7,33 @@ var startQuizButton = document.querySelector("#start-quiz");
 
 var questions = [
     {
-        title: "", 
-        choices: [], 
-        answer: 'answer here'
+        title: "Commonly used data types DO NOT include:", 
+        choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"], 
+        answer: "3. alerts",
     }, 
     {
-        title: "", 
-        choices: []
+        title: "The condition in an if / else statement is enclosed within ____.", 
+        choices: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        answer: "2. curly brackets",
     }, 
     {
-        title: "", 
-        choices: []
+        title: "Arrays in JavaScript can be used to store _____.", 
+        choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        answer: "4. all of the above",
+    },
+    {
+        title: "String values must be enclosed within _____ when being assigned to variables.", 
+        choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+        answer: "3. quotes",
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:", 
+        choices: ["1. JavaScript", "2. terminal / bash", "3. for loops", "4. console.log"],
+        answer: "4. console.log",
     }
-]
+];
+
+
 
 // timer.setAttribute("style", "float: right;");
 
